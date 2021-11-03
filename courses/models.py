a5b9ab8c75b93f2312dc1560459d12bccd63e2cd
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-11-03 09:29:08 +0300

    Many to Many Relation

--- courses/models.py
+++ courses/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 
 class Category(models.Model):
-  name = models.CharField(max_length=50, null=True)
-  slug = models.SlugField(max_length=50, unique=True, null=True)
+  name = models.CharField(max_length=100, null=True)
+  slug = models.SlugField(max_length=100, unique=True, null=True)
 
   def __str__(self):
     return self.name
 
+class Tag(models.Model):
+  name = models.CharField(max_length=100, null=True)
+  slug = models.SlugField(max_length=100, unique=True, null=True)
+
+  def __str__(self):
+    return self.name
 
 class Course(models.Model):
-  name = models.CharField(max_length=20, unique=True)
+  name = models.CharField(max_length=100, unique=True)
   category = models.ForeignKey(Category, null=True, on_delete=models.DO_NOTHING)
+  tags = models.ManyToManyField(Tag, blank=True, null=True)
   description = models.TextField(blank=True, null=True)
   image = models.ImageField(upload_to="courses/%Y/%m/%d")
   date = models.DateTimeField(auto_now=True)

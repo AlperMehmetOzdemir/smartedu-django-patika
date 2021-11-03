0b42f1ff0d1f33dd8b08ba1639e5ef1ca4b851f2
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-11-03 23:37:31 +0300

    Class based views 2

--- teachers/views.py
+++ teachers/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic import DetailView
+from courses.models import Course
 from teachers.models import Teacher
 
 
 class TeacherListView(ListView):
   model = Teacher
   template_name = "teachers.html"
-  
+
   def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
     context["teachers"] = Teacher.objects.all().order_by("name")
     return context
   
+class TeacherDetailView(DetailView):
+  model = Teacher
+  template_name="teacher.html"
+  context_object_name = "teacher"
 
-# def teacher_list(request):
-#   teachers = Teacher.objects.all().order_by("name")
-
-#   context = {
-#     "teachers": teachers,
-#   }
-
-#   return render(request, "teachers.html", context)
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context["courses"] = Course.objects.filter(available=True, teacher=self.kwargs["pk"] ).order_by("date")
+    return context

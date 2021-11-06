40d0a72ff4217208ab1cc739a44c6d7aac348174
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-11-06 16:38:10 +0300

    Course enrollment

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from accounts.forms import LoginForm, RegisterForm
+from courses.models import Course
 
 def user_login(request):
   if request.method == "POST":
@@ -49,4 +51,21 @@ def user_logout(request):
 @login_required(login_url="login")
 def user_dashboard(request):
   current_user = request.user
-  return render(request, "dashboard.html", current_user)
+  courses = current_user.courses_joined.all();
+
+  context = {
+    "user": current_user,
+    "courses": courses
+  }
+
+  return render(request, "dashboard.html", context)
+
+@login_required(login_url="login")
+def enroll_the_course(request):
+  course_id = request.POST["course_id"]
+  user_id = request.POST["user_id"]
+  course = Course.objects.get(id = course_id)
+  user = User.objects.get(id= user_id);
+  course.students.add(user)
+  
+  return redirect("dashboard")

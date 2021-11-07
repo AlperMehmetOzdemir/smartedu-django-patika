a0ed9c0b2c54ef8fa110b16c57fe903932290093
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-11-07 15:52:40 +0300

    Course finished

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,13 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
-from courses.models import Course
-from pages.forms import ContactForm
 from django.urls import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
+
+from pages.forms import ContactForm
+from courses.models import Course
+from teachers.models import Teacher
 
 class IndexView(TemplateView):
   template_name = "index.html"
@@ -13,6 +16,8 @@ class IndexView(TemplateView):
     context = super().get_context_data(**kwargs)
     context["courses"] = Course.objects.filter(available=True).order_by("-date")[:2]
     context["total_courses"] = Course.objects.filter(available=True).count()
+    context["total_users"] = User.objects.count()
+    context["total_teachers"] = Teacher.objects.count()
 
     return context
 

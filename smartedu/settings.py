de8777077970f137b75afd2e4645895e9a4d77df
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-11-08 09:42:50 +0300

    Heroku deployment conf

--- smartedu/settings.py
+++ smartedu/settings.py
@@ -12,11 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fo=oyp2e2#j=+&$c&kl&kd019d-m%vl59%eqwr-l3r#-@uli6u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
-
+ALLOWED_HOSTS = ["smartedu-django.herokuapp.com", "127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
